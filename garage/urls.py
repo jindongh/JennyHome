28570d0151bf6654bdd02c0e42845ada48aeebbe
Jindong Huang <jindongh@gmail.com>
2017-09-05 01:39:15 +0000

    Support color in garage page

--- garage/urls.py
+++ garage/urls.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     url(r'^$', 'garage.views.home', name='home'),
-    url(r'^api/status', 'garage.views.status', name='status'),
+    url(r'^api/state', 'garage.views.state', name='state'),
     url(r'^api/toggle', 'garage.views.toggle', name='toggle'),
 )
