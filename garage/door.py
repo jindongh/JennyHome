bfc8502d0304f4cdafc2ee43b8f5d88c0914b0ef
Jindong Huang <jindongh@gmail.com>
2017-09-10 02:45:25 +0000

    Refactor door operation, add door image display

---
+++ garage/door.py
@@ -0,0 +1,49 @@
+from django.conf import settings
+from models import GarageOp
+import cv2, numpy
+
+DOOR_OPEN = 'Open'
+DOOR_CLOSE = 'Close'
+DOOR_UNKNOWN = 'Unknown'
+
+def toggleDoor():
+    curState = getDoorStateFromDB()
+    # TODO
+    newState = toggleDoorState(curState)
+    return newState
+
+def getDoorStateFromDB():
+    history = GarageOp.objects.order_by('-op_date')[:1]
+    if len(history) == 0:
+        return DOOR_CLOSE
+    else:
+        return history[0].op_type
+
+def getDoorStateFromCV():
+    image = getDoorImage()
+    img_array = numpy.asarray(bytearray(image), dtype=numpy.uint8)
+    img = cv2.imdecode(img_array, cv2.CV_LOAD_IMAGE_COLOR)
+    height, width, channels = img.shape
+    mask = numpy.zeros((height+2, width+2), numpy.uint8)
+    start_pixel = (640, 310)
+    diff = (3,3,3)
+    retval, rect = cv2.floodFill(img, mask, start_pixel, (0,255,0), diff, diff)
+    if retval > 10000:
+        return DOOR_CLOSE
+    else:
+        return DOOR_OPEN
+
+def toggleDoorState(curState):
+    if curState == DOOR_OPEN:
+        return DOOR_CLOSE
+    else:
+        return DOOR_OPEN
+
+def getDoorImage():
+    blink = settings.BLINK
+    network = blink.network('Home')
+    camera = blink.camera(network, 'Garage')
+    thumb = blink.capture_thumbnail(camera)
+    image = blink.download_thumbnail(thumb)
+    return image
+
--- garage/views.py
+++ garage/views.py
@@ -4,21 +4,16 @@ from models import GarageOp
 from django.http import HttpResponse
 from django.http import JsonResponse
 from datetime import datetime
-from django.conf import settings
-import cv2
-import numpy
 import logging
+import door
 logger = logging.getLogger(__name__)
-DOOR_OPEN = 'Open'
-DOOR_CLOSE = 'Close'
-DOOR_UNKNOWN = 'Unknown'
 
 # Create your views here.
 @login_required
 def home(request):
     history = GarageOp.objects.order_by('-op_date')[:10]
-    curState = _getDoorStateFrom(history)
-    newState = _getNextDoorState(curState)
+    curState = door.getDoorStateFromDB()
+    newState = door.toggleDoorState(curState)
     context = {
             'history': history,
             'state': curState,
@@ -28,9 +23,8 @@ def home(request):
 
 @login_required
 def state(request):
-    stateByDB = _getDoorStateFromDB()
-    stateByCV = _getDoorStateFromCV()
-
+    stateByDB = door.getDoorStateFromDB()
+    stateByCV = door.getDoorStateFromCV()
     return JsonResponse({
         'stateFromDB': stateByDB,
         'stateFromCV': stateByCV
@@ -39,11 +33,10 @@ def state(request):
 @login_required
 def toggle(request):
     logger.info('Try to toggle garage door')
-    newState = DOOR_UNKNOWN
     succeed = False
     message = ''
     try:
-        newState = _toggleDoorState()
+        newState = door.toggleDoor()
         succeed = True
         logger.info('Garage door is ' + newState)
     except Exception, e:
@@ -63,51 +56,7 @@ def toggle(request):
         'error': op.error_message
         })
 
-def _toggleDoorState():
-    curState = _getDoorStateFromDB()
-    newState = _getNextDoorState(curState)
-    # Change door with RPi.GPIO
-    return newState
-
-def _getNextDoorState(curState):
-    if curState == DOOR_OPEN:
-        return DOOR_CLOSE
-    else:
-        return DOOR_OPEN
-
-def _getDoorStateFrom(history):
-    if len(history) == 0:
-        return DOOR_CLOSE
-    else:
-        return history[0].op_type
-
 @login_required
 def doorImage(request):
-    image = _getDoorImage()
+    image = door.getDoorImage()
     return HttpResponse(image, content_type="image/jpeg")
-
-def _getDoorStateFromCV():
-    image = _getDoorImage()
-    img_array = numpy.asarray(bytearray(image), dtype=numpy.uint8)
-    img = cv2.imdecode(img_array, cv2.CV_LOAD_IMAGE_COLOR)
-    height, width, channels = img.shape
-    mask = numpy.zeros((height+2, width+2), numpy.uint8)
-    start_pixel = (640, 310)
-    diff = (3,3,3)
-    retval, rect = cv2.floodFill(img, mask, start_pixel, (0,255,0), diff, diff)
-    if retval > 10000:
-        return DOOR_CLOSE
-    else:
-        return DOOR_OPEN
-
-def _getDoorImage():
-    blink = settings.BLINK
-    network = blink.network('Home')
-    camera = blink.camera(network, 'Garage')
-    thumb = blink.capture_thumbnail(camera)
-    image = blink.download_thumbnail(thumb)
-    return image
-
-def _getDoorStateFromDB():
-    history = GarageOp.objects.order_by('-op_date')[:1]
-    return _getDoorStateFrom(history)
